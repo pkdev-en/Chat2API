c0ec8dca1d932d4668e86213ba1d0be2ee37cfb0
axd <pkdevvn@outlook.com>
2026-07-25 07:59:21 +0700

    Add Package.swift for Chat2API_iOS project

---
+++ ios/Package.swift
@@ -0,0 +1,20 @@
+// --- ios/Package.swift ---
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "Chat2API_iOS",
+    platforms: [.iOS(.v16)],
+    products: [
+        .executable(name: "Chat2API_iOS", targets: ["Chat2API_iOS"])
+    ],
+    targets: [
+        .executableTarget(
+            name: "Chat2API_iOS",
+            path: "Chat2API_iOS",
+            swiftSettings: [
+                .unsafeFlags(["-parse-as-library"])
+            ]
+        )
+    ]
+)
